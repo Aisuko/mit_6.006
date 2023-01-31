e7fcaba4301cc9d4565768d41121412f586a160f
Aisuko <urakiny@gmail.com>
2023-01-31 00:43:38 +0000

    Add merge sort and  insertation sort(in place)
    Signed-off-by: Aisuko <urakiny@gmail.com>

--- 02_code/docdist1.py
+++ 02_code/docdist1.py
@@ -8,24 +8,24 @@ def count_frequency(word_list):
     L = []
     for new_word in word_list:
         for entry in L:
-            if new_word==entry[0]:
+            if new_word ==entry[0]:
                 entry[1]+=1
                 break
-            else:
-                L.append([new_word,1])
+        else:
+            L.append([new_word,1])
     return L
 
 
 
 ###################################
-# Operation 4: sort words into alphabetic order
+# Operation 4: sort words into alphabetic order(inplace)
 ###################################
 def insertion_sort(A):
     """
-    Sort list A into order, in place
+    Sort list A into order, in place and it uses 0-indexing
     """
-    s=len(A)
-    for j in range(s):
+    a_length=len(A)
+    for j in range(a_length):
         key=A[j]
         i=j-1
         while i>-1 and A[i]>key:
@@ -34,9 +34,50 @@ def insertion_sort(A):
         A[i+1]=key
     return A
 
+##################################################
+# Operation: 4: sort words into alphabetic order #
+##################################################
+def merge_sort(A):
+    """
+    Sort list A into order, and return result
+    """
+    n=len(A)
+    if n==1:
+        return A
+    mid=n//2 #floor division
+    L=merge_sort(A[:mid])
+    R=merge_sort(A[mid:])
+    return merge(L,R)
 
-# cf=count_frequency([1,1,1,2,3,4,5,5])
-# print(cf)
+
+def merge(L,R):
+    """
+    Given two sorted sequences L and R, return their merge
+    """
+    i=0
+    j=0
+    answer=[]
+    while i<len(L) and j<len(R):
+        if L[i]<R[j]:
+            answer.append(L[i])
+            i+=1
+        else:
+            answer.append(R[j])
+            j+=1
+    if i<len(L):
+        answer.extend(L[i:])
+    if j<len(R):
+        answer.extend(R[j:])
+    return answer
+
+
+
+
+cf=count_frequency([1,1,1,2,3,4,5,5])
+print(cf)
 
 a=insertion_sort([2,7,1,0,5])
 print(a)
+
+b=merge_sort([2,2,7,1,0,5,3,4,8])
+print(b)
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # mit_6.006
 Repo for code from MIT open-course 6.006
+
+
+
+The one about the `__main__` function's debugging in VSCode
+https://github.com/microsoft/debugpy/issues/1175#issuecomment-1408389901
