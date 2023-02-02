14e8d5fda6001ae3d6a23d21d6ad7cf99b2cd654
Aisuko <urakiny@gmail.com>
2023-02-02 06:58:52 +0000

    add bst a revised version
    Signed-off-by: Aisuko <urakiny@gmail.com>

---
+++ 06_code/README.md
@@ -0,0 +1,7 @@
+## 06_code
+
+###
+- [ ] Vanilla(unbalanced) BSTs
+      * It is a revised version of BST
+- [ ] AVL(balanced) tree 
+
---
+++ 06_code/bst.py
@@ -0,0 +1,64 @@
+#!/usr.bin/env python
+
+class BSTNode(object):
+    def __init__(self, parent, k) -> None:
+        self.key=k
+        self.parent=parent
+        self.left=None
+        self.right=None
+    def _str(self):
+        """Internal method for ASCII art."""
+        label = str(self.key)
+        if self.left is None:
+            left_lines, left_pos, left_width = [], 0, 0
+        else:
+            left_lines, left_pos, left_width = self.left._str()
+        if self.right is None:
+            right_lines, right_pos, right_width = [], 0, 0
+        else:
+            right_lines, right_pos, right_width = self.right._str()
+        middle = max(right_pos + left_width - left_pos + 1, len(label), 2)
+        pos = left_pos + middle // 2
+        width = left_pos + middle + right_width - right_pos
+        while len(left_lines) < len(right_lines):
+            left_lines.append(' ' * left_width)
+        while len(right_lines) < len(left_lines):
+            right_lines.append(' ' * right_width)
+        if (middle - len(label)) % 2 == 1 and self.parent is not None and \
+           self is self.parent.left and len(label) < middle:
+            label += '.'
+        label = label.center(middle, '.')
+        if label[0] == '.': label = ' ' + label[1:]
+        if label[-1] == '.': label = label[:-1] + ' '
+        lines = [' ' * left_pos + label + ' ' * (right_width - right_pos),
+                 ' ' * left_pos + '/' + ' ' * (middle-2) +
+                 '\\' + ' ' * (right_width - right_pos)] + \
+          [left_line + ' ' * (width - left_width - right_width) + right_line
+           for left_line, right_line in zip(left_lines, right_lines)]
+        return lines, pos, width
+    def __str__(self) -> str:
+        return '\n'.join(self._str()[0])
+    def find(self, k):
+        """Finds and returns the node with key k from the subtree tooted at this node.
+        K is the node we want to find
+        return the node with key k
+        """
+        if k==self.key:
+            return self
+        elif k<self.key:
+            if self.left.key is None:
+                return None
+            else:
+                return self.left.find(k)
+        else:
+            if self.right is None:
+                return None
+            else:
+                return self.right.find(k)
+    def find_min(self):
+        """Finds the node with minimum key in the subtree rooted at this node.
+        Returns: The node with minimum key.
+        """
+        
+
+        
