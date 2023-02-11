6e1a5fe68de154a87bcd26820d89063937d9aae3
Aisuko <urakiny@gmail.com>
2023-02-11 12:19:00 +0000

    Add valid anagram and ransom_note
    Signed-off-by: Aisuko <urakiny@gmail.com>

--- data_structure/array/242.valid_anagram.py
+++ data_structure/array/242.valid_anagram.py
@@ -3,8 +3,17 @@
 import collections
 class Solution:
     def isAnagram(self, s, t):
-        pass
+        """
+        O(len(s)+len(t)+len(dic.items()))
+        """
+        dic=collections.defaultdict(int)
+        for i in s:
+            dic[i]+=1
+        for i in t:
+            dic[i]-=1
+        #TODO
 
 
 if __name__=="__main__":
     print(Solution().isAnagram("anagram","nagaram"))
+    print(Solution().isAnagram("rat","car"))
--- data_structure/array/383.ransom_note.py
+++ data_structure/array/383.ransom_note.py
@@ -5,6 +5,27 @@ class Solution:
     def canConstruct(self,ransomNote,magazine):
         """
         How to check str1 includes str2?
+
+        Time complexity: O(m+n)
         """
-        pass
+        # even we use hash table, there is not necessary key
+        dic={}
+        for i in ransomNote:
+            if i in dic:
+                dic[i]+=1
+            else:
+                dic[i]=1
+        for i in magazine:
+            if i in dic:
+                # According to the request, 
+                # * "aa" ->"aab" is true.
+                # * "aa" -> "ab" is false
+                
+                #TODO
+
+
+if __name__ == "__main__":
+    print(Solution().canConstruct("a","b"))
+    print(Solution().canConstruct("aa","ab"))
+    print(Solution().canConstruct("aa","aab"))
         
