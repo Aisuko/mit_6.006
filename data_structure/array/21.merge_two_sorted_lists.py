25c9c0bc047edee61301614e21ea6b9dd4649557
Aisuko <urakiny@gmail.com>
2023-02-12 01:13:26 +0000

    Add merge two sorted lists
    Signed-off-by: Aisuko <urakiny@gmail.com>

---
+++ data_structure/array/21.merge_two_sorted_lists.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# 21. Merge Two Sorted Lists
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        """
+        l1 and l2 are sorted in non-decreasing order is useful
+        Time:
+        O(m+n)
+        """
+        # The basic situation needs to check
+        if not list1 or not list2:
+            return list1 or list2
+        # Compare their value and recursive
+        if list1.val<list2.val:
+            list1.next=self.mergeTwoLists(list1.next,list2)
+            return list1
+        else:
+            list2.next=self.mergeTwoLists(list1,list2.next)
+            return list2
+
+        # iterative solution
+        # tempHead=cur=ListNode(0)
+        # while list1 and list2:
+        #     if list1.vale<list2.val:
+        #         cur.next=list1
+        #         list1=list1.next
+        #     else:
+        #         cur.next=list2
+        #         list2=list2.next
+        #     cur=cur.next
+        
+        # cur.next =list1 or list2
+        # return tempHead.next
