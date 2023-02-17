24deb2101c14fa94e8a866d1f006db3e063865e7
Aisuko <urakiny@gmail.com>
2023-02-17 04:53:13 +0000

    Add stack and queue
    Signed-off-by: Aisuko <urakiny@gmail.com>

--- data_structure/stack&queue/20.valid_parentheses.py
+++ data_structure/stack&queue/20.valid_parentheses.py
@@ -4,4 +4,24 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         """
+        What we should pay attention on it?
+        * the structure for string is '(){}[]' which is as the couple,so we can use stack(LIFO) to save the data.
+        * we can simulate stack by using list[] in Python
         """
+        stack=[]
+        for i in s:
+            if i in ('(','{','['):
+                stack.append(i)
+            # if i is the right-side parentheses, 
+            # and we need to make sure it's left-side is the last element that we append to stack
+            elif i==')' and len(stack)!=0 and stack[-1]=='(':
+                # because it is the parentheses, we cannot compare it them directly,
+                # so we can count the length of stack
+                stack.pop()
+            elif i==']' and len(stack)!=0 and stack[-1]=='[':
+                stack.pop()
+            elif i=='}' and len(stack)!=0 and stack[-1]=='{':
+                stack.pop()
+            else:
+                return False
+        return len(stack)==0
--- data_structure/stack&queue/232.implement_queue_using_stacks.py
+++ data_structure/stack&queue/232.implement_queue_using_stacks.py
@@ -1,3 +1,37 @@
 #!/usr/bin/env python
 # 232.Implement Queue using Stacks
 
+class MyQueue:
+
+    def __init__(self):
+        # we use python, so we use list to simulate queue
+        self._data=[]
+        
+
+    def push(self, x: int) -> None:
+        """
+        Keep in mind the push operation, add new element to the queue
+        """
+        self._data.append(x)
+        
+
+    def pop(self) -> int:
+        """
+        Because of queue, FIFO, return the first element
+        """
+        return self._data.pop(0)
+        
+
+    def peek(self) -> int:
+        """
+        return peek, but does not change the items in the queue
+        """
+        return self._data[0]
+        
+
+    def empty(self) -> bool:
+        """
+        if len(self.data)>0 empty is false
+        else empty is False
+        """
+        return not self._data
