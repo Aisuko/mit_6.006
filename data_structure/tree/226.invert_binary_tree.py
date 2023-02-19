df14afb7e1dc25d48c81d798c8e386e4d4d6e57d
Aisuko <urakiny@gmail.com>
2023-02-19 05:27:54 +0000

    Add invert binary tree
    Signed-off-by: Aisuko <urakiny@gmail.com>

---
+++ data_structure/tree/226.invert_binary_tree.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# 226.Invert Binary Tree
+
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def invertTree(self, root):
+        """
+        iteration or recursive
+        """
+
+        # How to do this?
+        # the root node is still root,so get the root node first
