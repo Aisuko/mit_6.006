3e8dc87e7d413df768f251623cc2bcac3eb36dd3
Aisuko <urakiny@gmail.com>
2023-02-19 05:54:11 +0000

    Add path sum
    Signed-off-by: Aisuko <urakiny@gmail.com>

---
+++ data_structure/tree/112.path_sum.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# 112.Path Sum
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def hasPathSum(self, root, targetSum):
+        """
+        We need to calculate every path for the whole tree, and comapre them to the target
+        How to calculate the value for the path?
+        """
+        if not root:
+            return False
+        # why we use stack? 1.It is better do not change the value of parameter. 2, We want to use the interface from ADS
