19d2dcd1a8a23ad8b377aeab7f2102a6e654ddba
Aisuko <urakiny@gmail.com>
2023-02-21 04:05:06 +0000

    Add merge sort
    Signed-off-by: Aisuko <urakiny@gmail.com>

---
+++ sort_algorithms/merge_sort.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+#
+# Best     O(nlogn)
+# Average  O(nlogn)
+# Worst    O(nlogn)
+# Momery   1
+# Stable   Yes
+# Method   Insertion
+
+
+###########################
+# Insertion is more effective for small part of data, 
+# so we shold split data at first.
+# 
+# At the second step we need to check two side's elements one by one.
+
+
+def merge(left,right):
+    pass
+
+def merge_sort(m):
+    pass
+
+
+la=[3,5,4,6,12,8,2,1]
+
+print(merge_sort(la))
