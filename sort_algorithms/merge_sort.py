b72c258e1d86ce5b767c4f0af838a09a217b8ec7
Aisuko <urakiny@gmail.com>
2023-02-21 04:53:03 +0000

    add merge sort
    Signed-off-by: Aisuko <urakiny@gmail.com>

--- sort_algorithms/merge_sort.py
+++ sort_algorithms/merge_sort.py
@@ -16,10 +16,42 @@
 
 
 def merge(left,right):
-    pass
+    """
+    Use comparising
+    """
+    result=[]
+    l,r=0,0
+    while l<len(left) and r<len(right):
+        if left[l]<=right[r]:
+            result.append(left[l])
+            l+=1
+        else:
+            result.append(right[r])
+            r-=1
+    if left:
+        result.extend(left[l:])
+    if right:
+        result.extend(right[r:])
+    return result
+
+
 
 def merge_sort(m):
-    pass
+    """
+    Split the data into two part first
+    """
+    # check if the length for m is <=1 and then split the list into two part
+    if len(m)<=1:
+        return m
+    # how to split it into two parts, module 
+    middle=len(m)//2
+    left=m[:middle]
+    right=m[middle:]
+
+    # Do not forget sort left and right
+    left=merge_sort(left)
+    right=merge_sort(right)
+    return merge(left,right)
 
 
 la=[3,5,4,6,12,8,2,1]
