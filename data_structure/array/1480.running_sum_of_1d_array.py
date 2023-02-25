9b545f239bb27a4e6071ecad0857e907f685efc8
Aisuko <urakiny@gmail.com>
2023-02-25 05:39:54 +0000

    Add running sum of 1d array
    Signed-off-by: Aisuko <urakiny@gmail.com>

--- data_structure/array/1480.running_sum_of_1d_array.py
+++ data_structure/array/1480.running_sum_of_1d_array.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python
+# 1480. Running Sum of 1d Array
+
+class Solution:
+    def runningSum(self, nums):
+        if not nums:
+            return nums
+        res=[]
+        cur=0
+        for i in nums:
+            cur+=i
+            res.append(cur)
+        return res
