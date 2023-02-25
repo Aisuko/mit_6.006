0b50b6ba3bb49c3a8270df2789857d539dab0f11
Aisuko <urakiny@gmail.com>
2023-02-25 10:51:15 +0000

    Add isSubsequence
    Signed-off-by: Aisuko <urakiny@gmail.com>

---
+++ data_structure/string/392.is_subsequence.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# 392. Is Subdequence
+
+class Solution:
+    def isSubdequence(self,s,t):
+        """
+        Return bool
+        """
+        # if s is "", return True
+        if not s:
+            return True
+        elif not t:
+            return False
+        # isSubdequence(abc,ahbgdc)
+        #  isSubsequence(bc,hbgdc)
+        #   isSubsequence(bc,bgdc)
+        #    isSubsequence(c,gdc)
+        #     isSubsequence(c,dc)
+        #      isSubsequence(c,c)
+        #       isSubsequence("","")
+        return self.isSubdequence(s[1:],t[1:]) if s[0]==t[0] else self.isSubdequence(s,t[1:])
