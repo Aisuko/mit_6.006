178fe25ed536968ada24610cf2914de52ff24c51
Aisuko <urakiny@gmail.com>
2023-02-25 06:10:10 +0000

    find pivot index
    Signed-off-by: Aisuko <urakiny@gmail.com>

--- data_structure/array/724.find_pivot_index.py
+++ data_structure/array/724.find_pivot_index.py
@@ -1,1 +1,19 @@
 #!/usr/bin/env python
+# 724. Find Pivot Index
+
+class Soluction:
+    def pivotIndex(self, nums):
+        """
+        Return int
+        left ++
+        right--
+        left++=right--
+        """
+        left=0
+        right=sum(nums)
+        for k,v in enumerate(nums):
+            left+=v
+            if left==right:
+                return k
+            right-=v
+        return -1
