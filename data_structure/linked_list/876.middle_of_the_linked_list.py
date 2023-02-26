3032e291f476fbe6cb0b104d3f1e6a9d686e83c6
Aisuko <urakiny@gmail.com>
2023-02-26 23:39:30 +0000

    Add middle of the linked list
    Signed-off-by: Aisuko <urakiny@gmail.com>

--- data_structure/linked_list/876.middle_of_the_linked_list.py
+++ data_structure/linked_list/876.middle_of_the_linked_list.py
@@ -6,5 +6,12 @@ class Solution:
         """
         Return ListNode
         use the slow and fast pointers to count on the element
+        slow=slow.next
+        fast=head.next.next
+        so, if fast traversal to the end of the linked list, slow is must at the second middle element of the list
         """
-        
+        slow=fast=head
+        while fast and fast.next:
+            slow=slow.next
+            fast=fast.next.next
+        return slow
