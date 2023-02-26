1d3708920f7aaa252032d14992544e8e31397ada
Aisuko <urakiny@gmail.com>
2023-02-26 11:20:05 +0000

    Add 142 and 876
    Signed-off-by: Aisuko <urakiny@gmail.com>

---
+++ data_structure/linked_list/142.linked_list_cycle_ii.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# 142.Linked List Cycle II
+
+class Solution:
+    def detectCycle(self, head):
+        """
+        the length of fast pointer travel is equal to the slow pointer travel to the cycles' entry
+        """
---
+++ data_structure/linked_list/876.middle_of_the_linked_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# 876. Middle of the Linked List
+
+class Solution:
+    def middleNode(self, head):
+        """
+        Return ListNode
+        use the slow and fast pointers to count on the element
+        """
+        
