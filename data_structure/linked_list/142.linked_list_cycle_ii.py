ae4910cb3bef8abf9c00e81f76ac753954674185
Aisuko <urakiny@gmail.com>
2023-02-26 23:44:55 +0000

    Add linked list cycle
    Signed-off-by: Aisuko <urakiny@gmail.com>

--- data_structure/linked_list/142.linked_list_cycle_ii.py
+++ data_structure/linked_list/142.linked_list_cycle_ii.py
@@ -5,4 +5,16 @@ class Solution:
     def detectCycle(self, head):
         """
         the length of fast pointer travel is equal to the slow pointer travel to the cycles' entry
+        This is also related to the slow and fast pointers traversal path
         """
+        slow=fast=head
+        while fast and fast.next:
+            slow=slow.next
+            fast=fast.next.next
+            if slow==fast:
+                newSlow=head
+                while (newSlow!=fast):
+                    newSlow=newSlow.next
+                    fast=fast.next
+                return newSlow
+        return None
