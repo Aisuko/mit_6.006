c5f28f61316a2fd205548a4dd9df7c8ad43fb147
Aisuko <urakiny@gmail.com>
2023-02-27 03:31:19 +0000

    Add isomaphic strings
    Signed-off-by: Aisuko <urakiny@gmail.com>

---
+++ data_structure/string/409.longest_palindrome.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# 409 Longest Palindrome
+
+class Solution:
+    def longestPalindrome(self,s):
+        """
+        Return int
+        """
+        #TODO
+
+        l=set()
+        for c in s:
+            if c not in l:
+                l.add(c)
+            else:
+                l.remove(c)
+        return len(s)-len(l)+1 if len(l)>0 else len(s)
+
+print(Solution().longestPalindrome("abccccdd"))
