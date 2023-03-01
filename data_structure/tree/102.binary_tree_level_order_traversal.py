ca29fbb8a1b3eac57bfd8f3381a8023d0a35bdbf
Aisuko <urakiny@gmail.com>
2023-03-01 12:00:56 +0000

    redo binary tree level order traversal
    Signed-off-by: Aisuko <urakiny@gmail.com>

--- data_structure/tree/102.binary_tree_level_order_traversal.py
+++ data_structure/tree/102.binary_tree_level_order_traversal.py
@@ -8,27 +8,6 @@ class Solution:
     def levelOrder(self, root): #List[List[int]]
         """
         """
-        if not root:
-            return root
-        # Our traversal should be like level by level, 
-        # and how to implement it in real word?
-        # a data structure can support traversal
-        queue=[root]
-        res=[]
-        while queue:
-            # we return list that includes all row
-            row=[]
-            # the length for queue is equal to the length of the row
-            l=len(queue)
-            while l>0:
-                curr=queue.pop(0)
-                row.append(curr)
-                if curr.left:
-                    queue.append(curr.left)
-                if curr.right:
-                    queue.append(curr.right)
-            # add all the elelemts which belong to same level
-            res.append(row)
-        return res
+        #TODO
 
 
