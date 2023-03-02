e91e07ad5530ddeec7c15fd5df05cc7b89ab3b12
Aisuko <urakiny@gmail.com>
2023-03-02 11:42:40 +0000

    Add binary search related question
    Signed-off-by: Aisuko <urakiny@gmail.com>

--- binary_search/278.first_bad_version.py
+++ binary_search/278.first_bad_version.py
@@ -1,2 +1,26 @@
 #!/usr/bin/env python
-# 94.Binary Tree Inorder Traversal
+# 278. First Bad Version
+
+# The isBadVersion API is already defined for you.
+def isBadVersion(version: int):
+    return True
+
+class Solution:
+    def firstBadVersion(self,n):
+        """
+        Return int
+        This question is a little bit make people feel confused
+        """
+        start=0
+        end=n
+        res=0
+        while start<=end:
+            middle=(start+end)//2
+            if isBadVersion(middle):
+                res=middle
+                end=middle-1
+            else:
+                start=middle+1
+        return res
+
+
