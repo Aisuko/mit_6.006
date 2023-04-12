84f78396c7cb41e03a91b2f6c544c34aafc2f4c3
Aisuko <urakiny@gmail.com>
2023-04-12 05:36:59 +0000

    Add code comment for question 121
    Signed-off-by: Aisuko <urakiny@gmail.com>

--- data_structure/array/121.best_time_to_buy_and_sell_stock.py
+++ data_structure/array/121.best_time_to_buy_and_sell_stock.py
@@ -11,8 +11,10 @@ def maxProfit(prices)->int:
     finalProfit=profit=k=0
     
     while k<len(prices)-1:
-        # Here we do not need to iterater every elements twice, please remember that the profit=(yesterday's profit+today's profit)
+        # Here we do not need to traversal every elements twice, please remember that the profit=(yesterday's profit+today's profit)
         # the latest profit= total profit until now compares to today's profit
+
+        # In this case, the minimal profit is 0. So, it will be wrong if you use max (temp,temp+today's profit)
         profit=max(0,profit+prices[k+1]-prices[k])
         finalProfit=max(profit,finalProfit)
         k+=1
